9716112d6f3cc06893f006c4fcc81128e1a788bf
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-07-23 15:27:26 +0200

    insert button.

--- guessing_game/guess.js
+++ guessing_game/guess.js
@@ -1,32 +1,34 @@
-let maximum = parseInt(prompt("Enter the maximum number!"));
+// JavaScript
+function startGame() {
+  let maximum = parseInt(prompt("Enter your max-number!"));
+  while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+  }
 
-while (!maximum) {
-  maximum = parseInt(prompt("Enter a valid number!"));
-}
+  const targetNum = Math.floor(Math.random() * maximum) + 1;
 
-const targetNum = Math.floor(Math.random() * maximum) + 1;
+  let guess = parseInt(prompt("Enter your first guess:"));
+  let attempts = 1;
 
-let guess = parseInt(prompt("Enter your first guess:"));
-let attempts = 1;
-
-while (parseInt(guess) !== targetNum) {
-  if (guess === "quit") break;
-  attempts++;
-  if (guess > targetNum) {
-    guess = prompt(
-      "Too high! Enter new guess: (you can quit the game by typing 'quit')"
-    );
+  while (parseInt(guess) !== targetNum) {
+    if (guess === "quit") break;
+    attempts++;
+    if (guess > targetNum) {
+      guess = prompt(
+        "Too high! Enter new guess: (you can quit the game by typing 'quit')"
+      );
+    } else {
+      guess = prompt(
+        "Too low! Enter a new guess: (you can quit the game by typing 'quit')"
+      );
+    }
+  }
+  if (guess === "quit") {
+    alert("OK, You Quit");
   } else {
-    guess = prompt(
-      "Too low! Enter a new guess: (you can quit the game by typing 'quit')"
-    );
+    alert("Congrats you win!");
+
+    let answerElement = document.getElementById("winner");
+    answerElement.innerText = `WINNER\n Your guess ${guess} is right!! \n You tried it ${attempts} times`;
   }
 }
-if (guess === "quit") {
-  alert("OK, You Quit");
-} else {
-  alert("Congrats you win!");
-}
-
-let answerElement = document.getElementById("winner");
-answerElement.innerText = `WINNER\n Your guess ${guess} is right!! \n You tried it ${attempts} times`;
