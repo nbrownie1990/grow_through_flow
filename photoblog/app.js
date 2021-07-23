74af99fb16e032d5b196cbce547efa9d12339e0d
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-07-23 18:13:51 +0200

    insert id-message.

--- photoblog/app.js
+++ photoblog/app.js
@@ -4,6 +4,9 @@ panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClasses();
     panel.classList.add("active");
+    document.getElementById(
+      "nav"
+    ).innerText = `Hey ${panel.id} - you are looking soo good!`;
   });
 });
 
