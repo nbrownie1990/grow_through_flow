11938f0f207d70fd60f39ee6482e96932551d2c2
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-07-23 12:00:14 +0200

    while & if-statements for guessing.

--- guessing_game/guess.js
+++ guessing_game/guess.js
@@ -3,5 +3,17 @@ let maximum = parseInt(prompt("Enter the maximum number!"));
 while (!maximum) {
   maximum = parseInt(prompt("Enter a valid number!"));
 }
+
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess:"));
+
+while (guess !== targetNum) {
+  if (guess > targetNum) {
+    guess = parseInt(prompt("Too high! Enter new guess:"));
+  } else {
+    guess = parseInt(prompt("Too low! Enter a new guess:"));
+  }
+}
+alert("YOU GOT IT!!!");
