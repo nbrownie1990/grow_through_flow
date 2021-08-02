6cc823e82d5780fddd1b01d397d8126719c5ac73
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-08-02 17:24:19 +0200

    fixed the delete-btn issue.

--- grocery_list/events.js
+++ grocery_list/events.js
@@ -1,13 +1,24 @@
 const form = document.querySelector("form");
 let list = document.querySelector("#list");
+let listArea = document.querySelector(".listArea");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let newList = document.createElement("li");
   let prodTxt = document.querySelector("#product").value;
   let qtyTxt = document.querySelector("#qty").value;
+  const delButton = document.createElement("button");
+  delButton.innerHTML = '<i class="fas fa-times-circle"> </i>';
   newList.innerText = `${qtyTxt} ${prodTxt}`;
+  delButton.classList.add("delete-btn");
+  newList.appendChild(delButton);
   list.appendChild(newList);
   form.elements.product.value = "";
   form.elements.qty.value = "";
+});
+
+listArea.addEventListener("click", (e) => {
+  const item = e.target;
+  const toBuy = item.parentElement;
+  item.nodeName === "BUTTON" && toBuy.remove();
 });
