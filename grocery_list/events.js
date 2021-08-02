c5738f30d610cd14250f5143f42923296bac8f34
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-08-02 13:34:54 +0200

    first commit grocery-list.

---
+++ grocery_list/events.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form");
+let list = document.querySelector("#list");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let newList = document.createElement("li");
+  let prodTxt = document.querySelector("#product").value;
+  let qtyTxt = document.querySelector("#qty").value;
+  newList.innerText = `${qtyTxt} ${prodTxt}`;
+  list.appendChild(newList);
+  form.elements.product.value = "";
+  form.elements.qty.value = "";
+});
